69cb83f05ae8dfb012880c37eb9ba5335ac0e680
Bruno <brunofernandez@protonmail.com>
2021-01-18 19:05:42 -0300

    Se agregaron nuevos path

--- apps/registrarTransacciones/urls.py
+++ apps/registrarTransacciones/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import registrarTransaccion
+from .views import registrarTransaccion, listarTransacciones, registrarCliente, listarClientes
 
 urlpatterns = [
+    path('Clientes/', listarClientes, name='listaClientes'),
+    path('RegistrarCliente/', registrarCliente, name='registrarCliente'),
+    path('Transacciones/', listarTransacciones, name='listaTransacciones'),
     path('RegistrarTransaccion/', registrarTransaccion, name='registrarTransaccion'),
 ]
